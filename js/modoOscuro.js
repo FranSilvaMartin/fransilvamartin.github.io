01df966a3230d27cea8244a23a59660a3b3e9b04
Francisco Silva Martín <fransilvamartin@factoriabiz.com>
2022-04-27 19:15:47 +0200

    :art: index, accion y arcade habilitado el modo oscuro

--- js/modoOscuro.js
+++ js/modoOscuro.js
@@ -1,13 +1,44 @@
 function modoOscuro() {
     var element = document.getElementById("botonModoOscuro");
+    var titulo = document.getElementById("tituloInicio");
+    var texto = document.getElementsByClassName("textos");
+    var textoTitulo = document.getElementsByClassName("textoTitulo");
 
     if (element.className == "fas fa-toggle-off") {
         element.className = "fas fa-toggle-on";
-        element.style.color = "black";
-        document.body.style.backgroundColor = "black";
+        element.style.color = "#191919";
+        document.body.style.backgroundColor = "#191919";
+        titulo.style.color = "white";
+
+        var len = textoTitulo.length;
+        for(var i = 0; i < len; ++i) {
+            textoTitulo[i].style.color = '#3C91E6';
+        }  
+    
+        var len2 = texto.length;
+        for(var i = 0; i < len2; ++i) {
+            texto[i].style.color = 'white';
+        }   
+
+        var len3 = imagenes.length;
+        for(var i = 0; i < len3; ++i) {
+            imagenes[i].style.boxShadow = '';
+        }   
+
     } else {
         element.className = "fas fa-toggle-off";
         element.style.color = "white";
         document.body.style.backgroundColor = "white";
+        titulo.style.color = "black";
+
+        var len = textoTitulo.length;
+        for(var i = 0; i < len; ++i) {
+            textoTitulo[i].style.color = 'black';
+        }  
+    
+        var len2 = texto.length;
+        for(var i = 0; i < len2; ++i) {
+            texto[i].style.color = 'black';
+        }  
     }
 }
