9474c5569284ef5a644e169b220eea4a7ab45224
Francisco Silva Martín <fransilvamartin@factoriabiz.com>
2022-04-27 20:22:07 +0200

    :art: :bookmark: Proyecto terminado

---
+++ js/hoover.js
@@ -0,0 +1,6 @@
+function colorAmarillo(elemento) { 
+    document.getElementById(elemento).style.color = 'yellow';
+}
+function colorBlanco(elemento) { 
+    document.getElementById(elemento).style.color = 'white';
+}
